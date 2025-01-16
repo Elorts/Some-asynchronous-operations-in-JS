4624ed99c25e77da405c77ce0741e64da6c3e8e4
deividas strole <deiwis@yahoo.com>
2025-01-16 12:53:24 -0800

    Consuming Promises

--- starter/script.js
+++ starter/script.js
@@ -1,8 +1,8 @@
 'use strict';
 
 // Countries info app:
-// const btn = document.querySelector('.btn-country');
-// const countriesContainer = document.querySelector('.countries');
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
 
 // ///////////////////////////////////////
 // const getCountryAndNeighbour = function (country) {
@@ -10,31 +10,31 @@
 //   request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
 //   request.send();
 //   // AJAX call country 1
-//   const renderCountry = function (data, className = '') {
-//     const html = `
-//   <article class="country ${className}">
-//           <img class="country__img" src="${data.flags.png}" />
-//           <div class="country__data">
-//             <h3 class="country__name">${data.name.common}</h3>
-//             <h4 class="country__region">${data.region}</h4>
-//             <p class="country__row"><span>👫</span>${(
-//               +data.population / 1000000
-//             ).toFixed(1)} million people</p>
-//             <p class="country__row"><span>🗣️</span>${
-//               Object.values(data.languages)[0]
-//             }</p>
-//             <p class="country__row"><span>💰</span>${
-//               Object.values(data.currencies)[0].name
-//             }</p>
-//           </div>
-//         </article>
-//         `;
+const renderCountry = function (data, className = '') {
+  const html = `
+  <article class="country ${className}">
+          <img class="country__img" src="${data.flags.png}" />
+          <div class="country__data">
+            <h3 class="country__name">${data.name.common}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(
+              +data.population / 1000000
+            ).toFixed(1)} million people</p>
+            <p class="country__row"><span>🗣️</span>${
+              Object.values(data.languages)[0]
+            }</p>
+            <p class="country__row"><span>💰</span>${
+              Object.values(data.currencies)[0].name
+            }</p>
+          </div>
+        </article>
+        `;
 
-//     console.log(html);
+  console.log(html);
 
-//     countriesContainer.insertAdjacentHTML('beforeend', html);
-//     countriesContainer.style.opacity = 1;
-//   };
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
 
 //   request.addEventListener('load', function () {
 //     const [data] = JSON.parse(this.responseText);
@@ -67,5 +67,13 @@
 
 console.log(`~~~ Promises~~~`);
 
-const request = fetch(`https://restcountries.com/v3.1/name/portugal`);
-console.log(request);
+// const request = fetch(`https://restcountries.com/v3.1/name/portugal`);
+// console.log(request);
+
+const getCountryData = function (country) {
+  fetch(`https://restcountries.com/v3.1/name/${country}`)
+    .then(response => response.json())
+    .then(data => renderCountry(data[0]));
+};
+
+getCountryData('Portugal');
