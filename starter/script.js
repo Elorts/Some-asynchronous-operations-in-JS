7155ad6f18d2c4925eb51ab2c240d5cd07f513c4
deividas strole <deiwis@yahoo.com>
2025-01-18 13:45:41 -0800

    finished this bs

--- starter/script.js
+++ starter/script.js
@@ -348,7 +348,7 @@ const createImage = function (imgPath) {
     const img = document.createElement('img');
     img.src = imgPath;
     img.alt = 'Mountains';
-
+    console.log(`IMG loaded!`);
     img.addEventListener('load', function () {
       imgContainer.append(img);
       resolve(img);
@@ -365,6 +365,7 @@ const loadNPause = async function () {
     currentImg = img;
     await wait(2);
     currentImg.style.display = 'none';
+
     await createImage(`img/img-2.jpg`);
     currentImg = img;
     await wait(2);
@@ -374,22 +375,21 @@ const loadNPause = async function () {
   }
 };
 
-loadNPause();
+//loadNPause();
 
-// createImage(`img/img-1.jpg`)
-//   .then(img => {
-//     currentImg = img;
-//     return wait(2);
-//   })
-//   .then(() => {
-//     currentImg.style.display = 'none';
-//     return createImage(`img/img-2.jpg`);
-//   })
-// .then(img => {
-//   currentImg = img;
-//   return wait(2);
-// })
-// .then(() => {
-//   currentImg.style.display = 'none';
-// })
-// .catch(error => console.error(error));
+// 2.
+
+const loadAll = async function (imgArr) {
+  try {
+    const imgs = imgArr.map(async i => await createImage(i));
+    console.log(`imgs: ${imgs}`);
+
+    const imgsEl = await Promise.all(imgs);
+    console.log(imgsEl);
+    imgsEl.forEach(img => img.classList.add('parallel'));
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+loadAll(['img/img-1.jpg', 'img/img-2.jpg', 'img/img-3.jpg']);
